eefb81b8c390603cc6cd8ede03118e1aee74a85e
satya perisetty <125467928+satya123182@users.noreply.github.com>
2024-08-22 02:16:35 +0530

    Revert "a big change"
    This reverts commit c0b0c43b53a77ffef3fe8e950be75e4a2a9b5d6f.

--- routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { loginUser , loginDonor } from '../controllers/authController.js';
-
-const router = express.Router();
-
-router.post('/donorLogin', loginDonor);
-router.post('/userLogin', loginUser);
