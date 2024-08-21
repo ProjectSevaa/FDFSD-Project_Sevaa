72e85472bc5bf2f3233b1a986717b38a5b2875c8
satya perisetty <125467928+satya123182@users.noreply.github.com>
2024-08-22 02:17:22 +0530

    Reapply "second commit"
    This reverts commit bcd35ea0fa7d32bc2593e059fec8f22b2a56f02f.

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { addUser  } from "../controllers/userController.js";
-
-const router = express.Router();
-
-router.post('/addUser' , addUser);
-
-export default router;
