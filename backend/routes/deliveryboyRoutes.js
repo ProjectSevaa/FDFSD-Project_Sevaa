84cc5a2741fae4e25adf772f2b5a7ce81321f544
AbhiramKothagundu <abhikothagundu@gmail.com>
2024-12-04 01:35:19 +0530

    deli

--- backend/routes/deliveryboyRoutes.js
+++ backend/routes/deliveryboyRoutes.js
@@ -1,31 +1,31 @@
-import express from 'express';
-import { 
-    createDeliveryBoy, 
-    findNearbyPosts, 
-    getAllDeliveryBoys, 
-    getDeliveryBoysByUser,
-    getDeliveryBoyDashboard,
-    addDeliveryBoyToUser,
-    toggleStatus 
-} from '../controllers/deliveryboyController.js';
+import express from "express";
+import {
+  createDeliveryBoy,
+  findNearbyPosts,
+  getAllDeliveryBoys,
+  getDeliveryBoysByUser,
+  getDeliveryBoyDashboard,
+  addDeliveryBoyToUser,
+  toggleStatus,
+} from "../controllers/deliveryboyController.js";
 
 const router = express.Router();
 
 // Add a new delivery boy
-router.post('/createDeliveryBoy', createDeliveryBoy);
-router.post('/addDeliveryBoyToUser', addDeliveryBoyToUser);
+router.post("/createDeliveryBoy", createDeliveryBoy);
+router.post("/addDeliveryBoyToUser", addDeliveryBoyToUser);
 
 // Get all delivery boys assigned to a particular user
-router.get('/deliveryBoysByUser', getDeliveryBoysByUser);
+router.get("/deliveryBoysByUser", getDeliveryBoysByUser);
 
 // Get all delivery boys
-router.get('/getAllDeliveryBoys', getAllDeliveryBoys);
+router.get("/getAllDeliveryBoys", getAllDeliveryBoys);
 
 // Find nearby users for a delivery boy
-router.get('/findNearbyPosts', findNearbyPosts);
+router.get("/findNearbyPosts", findNearbyPosts);
 
-router.get('/getDeliveryBoyDashboard', getDeliveryBoyDashboard);
+router.get("/getDeliveryBoyDashboard", getDeliveryBoyDashboard);
 
-router.post('/toggle-status/:id', toggleStatus);
+router.post("/toggle-status/:id", toggleStatus);
 
 export default router;
